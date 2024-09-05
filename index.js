9c93c8f7a989bee7a2ec73b7e15d3f2d321896fe
Fernando Maldonado <fernando.maldonado@fing.edu.uy>
2024-09-04 23:57:07 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+const {dbConnection} = require('./database/config');
+//const cors = require('cors');
+
+//Crear el servidor de express
+const app = express();
+
+//Base de datos
+dbConnection();
+
+//CORS
+//app.use( cors);
+
+//Directorio publico
+app.use(express.static('public'));
+
+//Lectura y parseo del body
+app.use(express.json());
+
+//Rutas
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/events',require('./routes/events'));
+
+//TODO: CRUD: Eventos
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
